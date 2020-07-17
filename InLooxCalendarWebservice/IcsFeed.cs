25d8969f013f38b926f19d45025afe15f89a872f
Dr. Andreas Tremel <tremel@inloox.net>
2020-07-17 10:45:54 +0200

    Projektdateien hinzufügen.

---
+++ InLooxCalendarWebservice/IcsFeed.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Ical.Net;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
+using InLoox.ODataClient.Data.BusinessObjects;
+
+namespace InLooxCalendarWebservice
+{
+    public class IcsFeed
+    {
+        private const string INLOOX_DISPLAY_NAME = "InLoox";
+        private const string TASK_ENTITY_DISPLAY_NAME = "Task"; // <- change this if you like
+        private const string TIMEZONE_UTC = "UTC";
+        private const string STANDARD_FREE_KEY = "FBTYPE";
+        private const string MICROSOFT_FREE_KEY = "X-MICROSOFT-CDO-BUSYSTATUS";
+        private const string FREE_VALUE = "FREE";
+        private TimeSpan _reminderTimespan = TimeSpan.FromMinutes(-30); // <- change this if you like
+
+        private readonly IEnumerable<WorkPackageView> _items;
+
+        public IcsFeed(IEnumerable<WorkPackageView> items)
+        {
+            _items = items;
+        }
+
+        public string Serialize()
+        {
+            
+            // create calendar, Outlook will ignore the name
+            var calendar = new Ical.Net.Calendar
+            {
+                Name = INLOOX_DISPLAY_NAME
+            };
+
+            foreach (var item in _items)
+            {
+                // check if task item has dates
+                if (item.HasStartDate == true
+                    && item.HasEndDate == true)
+                {
+                    // add basic info
+                    var calendarItem = new CalendarEvent
+                    {
+                        Summary = item.Name,
+                        Start = new CalDateTime(((DateTimeOffset)item.StartDateTime).UtcDateTime, TIMEZONE_UTC),
+                        End = new CalDateTime(((DateTimeOffset)item.EndDateTime).UtcDateTime, TIMEZONE_UTC),
+                        Categories = new List<string> { INLOOX_DISPLAY_NAME, TASK_ENTITY_DISPLAY_NAME, item.PlanningReservationId.ToString() },
+                        Uid = item.PlanningReservationId.ToString(),
+                        Description = string.Empty
+                    };
+                    
+                    // add free/busy type
+                    calendarItem.AddProperty(STANDARD_FREE_KEY, FREE_VALUE);
+                    calendarItem.AddProperty(MICROSOFT_FREE_KEY, FREE_VALUE);
+
+                    // add alarm, Outlook does not support this
+                    var alarm = new Alarm()
+                    {
+                        Summary = item.Name,
+                        Trigger = new Trigger(_reminderTimespan),
+                        Action = AlarmAction.Display
+                    };
+                    calendarItem.Alarms.Add(alarm);
+
+                    // add to calendar
+                    calendar.Events.Add(calendarItem);
+                }
+            }
+            
+            // create ICS output
+            var serializer = new CalendarSerializer();
+            var serializedCalendar = serializer.SerializeToString(calendar);
+
+            return serializedCalendar;
+        }
+    }
+}
---
+++ InLooxCalendarWebservice/Startup.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Primitives;
+using System.Threading.Tasks;
+
+namespace InLooxCalendarWebservice
+{
+    public class Startup
+    {
+        private const string SERVICE_URL = "https://app.inlooxnow.com/"; // <- insert your IWA service url here in case you run InLoox on premise
+        private const string MY_TASKS_ROUTE = "my-tasks"; // <- change this if you like
+        private const string ACCESS_TOKEN_PARAMETER_NAME = "access_token";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // standard route
+                if (env.IsDevelopment())
+                {
+                    endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync("Running");
+                    });
+                }
+
+                // my tasks route
+                endpoints.MapGet("/" + MY_TASKS_ROUTE, async context =>
+                {
+                    await ProcessMyTasksRequest(context);
+                });
+            });
+        }
+
+        private static async Task<string> GetToken(HttpContext context)
+        {
+            // access token has to be a get parameter
+            // Outlook and others are unable to post parameters/headers
+            var token = context.Request.Query[ACCESS_TOKEN_PARAMETER_NAME];
+            if (token.Count == 0)
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized");
+            }
+
+            return token;
+        }
+
+        private static async Task ProcessMyTasksRequest(HttpContext context)
+        {
+            // use the token for authorization
+            var token = await GetToken(context);
+
+            // connect to service
+            var service = new IwaService();
+            if (await service.Connect(new System.Uri(SERVICE_URL), token))
+            {
+                // read user's tasks
+                var tasks = await service.GetMyTasks();
+
+                // create ICS feed and return
+                var feed = new IcsFeed(tasks);
+                await context.Response.WriteAsync(feed.Serialize());
+            }
+        }
+    }
+}
